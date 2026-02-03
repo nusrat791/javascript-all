9d47ab89023b094d996a47af4df659916e5d4e6a
nusrat791 <nusratjahantithi791@gmail.com>
2026-02-03 23:35:08 +0600

    add naiming

--- number.js
+++ number.js
@@ -1,3 +1,13 @@
 var orangeprice=90;
 var chocolate=0.7;
-console.log(orangeprice);
+var apple="7890";
+var appleprice=parseInt(apple);
+console.log( orangeprice + apple);
+console.log(typeof orangeprice);
+console.log(typeof chocolate);
+//console.log(typeof apple);
+//math needed for programming//
+var first=0.9;
+var second=1.7;
+var total=first+second;
+console.log(total.toFixed(3));
