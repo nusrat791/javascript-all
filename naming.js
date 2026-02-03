ae1096d5dd8691a6cd3c5560929a3ab4f506f96e
nusrat791 <nusratjahantithi791@gmail.com>
2026-02-03 21:00:47 +0600

    add more

---
+++ naming.js
@@ -0,0 +1,20 @@
+//no keyword in variable name//
+var false=67;
+// 2. no space or gap in variable name//
+var is happ=false;
+var ishappy=true;
+
+// 3. no quote in variable name//
+var 'name'="john";
+//4. variable name can not start with number//
+var 9price=89;
+//5. use camelcase for multiple word variable name//
+var my_name="john";
+var myName="john";
+//6. variable name can not be a keyword//
+var for=89;
+//how to wriite a valid variable name//
+var my current home address ="hduhbn djhdu new york";
+var my_current_home_address ="hduhbn djhdu new york";//snake case//
+var myCurrentHomeAddress ="hduhbn djhdu new york";//camel case//
+var MyCurrentHomeAddress ="hduhbn djhdu new york";//pascal case//
---
+++ number.js
@@ -0,0 +1,3 @@
+var orangeprice=90;
+var chocolate=0.7;
+console.log(orangeprice);
