cd325b1b20270aae721ee8f3c1501dfd6ee31bee
nusrat791 <nusratjahantithi791@gmail.com>
2026-02-04 13:44:05 +0600

    comparison

---
+++ compare.js
@@ -0,0 +1,6 @@
+
+//console.log(5 ==3); // true
+//console.log(2 <= 1);
+var peyara=990;
+var apple=56;
+console.log(peyara != apple);
--- js-conditionals/comapre.js
+++ js-conditionals/comapre.js
@@ -4,6 +4,10 @@
 // greater or equal:>=
 // less or equal:<=
 // equal:==
+//and ://&&
+//or :||
 
 // //
 
+console.log(5 > 3); // true
+console.log(2 < 1);
