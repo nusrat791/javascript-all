e0b41adc85658dedb3ede465ddc9ac47c4c798e0
nusrat791 <nusratjahantithi791@gmail.com>
2026-02-16 09:54:46 +0600

    prac more

---
+++ if-else.js
@@ -0,0 +1,12 @@
+ var biriyani=200;
+ if(biriyani<300){
+    console.log("i will eat biriyani");
+ }
+ //if(condition){
+ //  code to be executed if condition is true}
+ //else{
+ // code to be executed if condition is false}
+  
+ //
+
+ 
---
+++ multiple.js
@@ -0,0 +1,11 @@
+const salary=25000;
+const isbcs=true;
+const height=60;
+
+if(salary>20000 )
+{
+    console.log("i will buy a car");
+}
+else{
+    console.log("i will not buy a car");
+}
